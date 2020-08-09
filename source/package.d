0dcff4488b364ca340b176a38ee2fb8493414b11
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2020-08-09 22:05:22 +0200

    ADd package.d

---
+++ source/package.d
@@ -0,0 +1,22 @@
+/**
+Copyright: Copyright (c) 2020, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+*/
+module my;
+
+public import cstring;
+public import deref;
+public import file;
+public import filter;
+public import from_;
+public import fsm;
+public import fswatch;
+public import hash;
+public import parse;
+public import path;
+public import range;
+public import semver;
+public import set;
+public import stat;
+public import xdg;

8acbab77f46d0929b9e780a7428f26bd7ca6aa7c
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2020-11-09 20:59:50 +0100

    signal_theory: separate and generalize simulator

--- source/my/signal_theory/pid.d
+++ source/my/signal_theory/pid.d
@@ -286,6 +286,7 @@ private:
 @("shall instantiate a PositionPid")
 unittest {
     import std.stdio : writefln, writeln;
+    import my.signal_theory.simulate;
 
     Simulator sim;
 
@@ -303,82 +304,19 @@ unittest {
     pid.setKd(LowPassFilter(1, 10));
 
     while (sim.currTime < 1000.dur!"msecs") {
-        sim.tick!"nsecs"(pid);
-        if (sim.updatePid) {
+        sim.tick!("nsecs")(a => pid.input(a.total!"nsecs"), () => pid.output);
+        if (sim.updated) {
             const diff = sim.targetTime - sim.wakeupTime;
-            writefln!"time[%s] pv[%s] diff[%s] output:%0.2f"(sim.currTime, sim.pv, diff, pid.output);
-            writeln(pid);
+            //writefln!"time[%s] pv[%s] diff[%s] output:%0.2f"(sim.currTime, sim.pv, diff, pid.output);
+            //writeln(pid);
         }
     }
 
-    assert(sim.pv.total!"msecs" < 100);
-    assert(pid.output < 10000.0);
+    import std.math : abs;
+
+    assert(abs(sim.pv.total!"msecs") < 100);
+    assert(abs(pid.output) < 10000.0);
 }
-
-struct Simulator {
-    import std.random;
-
-    // how much we have drifted from the desired period.
-    Duration pv;
-
-    // The current time.
-    Duration currTime = 16666667.dur!"nsecs";
-    // Simulated length of a tick
-    Duration simTick = 100.dur!"nsecs";
-
-    // Last time the PV where updated.
-    Duration lastUpdate;
-    // Next time the PID should be updated
-    Duration wakeupTime = 100.dur!"nsecs";
-    // True if the PID should execute.
-    bool updatePid;
-
-    // The desired period.
-    Duration period = 16666667.dur!"nsecs";
-    // The target time that should be as close as possible to currTime.
-    // starting at -2ms simulate a static offset
-    Duration targetTime = 14666667.dur!"nsecs";
-
-    double gain0 = 0;
-
-    MinstdRand0 g = MinstdRand0(42);
-    double posRn() {
-        return uniform01(g);
-    }
-
-    double spreadRn() {
-        return uniform!"[]"(-1.0, 1.0, g);
-    }
-
-    void tick(string TsUnit = "nsecs", PidT)(ref PidT pid) {
-        currTime += simTick;
-        updatePid = false;
-
-        if (currTime < wakeupTime)
-            return;
-        pv = currTime - targetTime;
-        pid.input(pv.total!TsUnit);
-
-        double gain = spreadRn() * period.total!TsUnit / 10000;
-
-        if (posRn > 0.8) {
-            gain0 = spreadRn * period.total!TsUnit / 1000;
-        }
-        gain += gain0;
-
-        // simulate that high frequency jitter only sometimes occur.
-        if (posRn > 0.99) {
-            gain += posRn * period.total!TsUnit * 0.5;
-        }
-
-        // the output is a time delay and thus can never be negative.
-        wakeupTime = currTime + period + (cast(long)(pid.output + gain)).dur!TsUnit;
-
-        lastUpdate = targetTime;
-        targetTime += period;
-        updatePid = true;
-    }
-};
 
 /// A first order low pass filter.
 struct LowPassFilter {
